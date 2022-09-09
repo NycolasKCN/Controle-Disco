e39b33e8de1f908e96ca5a2ad28ab798d2ac3a43
Nycolas Kevin <nycolas.costa@dcx.ufpb.br>
2022-09-09 10:43:46 -0300

    add readFile; codigo funcional

--- src/CotasDeDisco/ControleDeDisco.java
+++ src/CotasDeDisco/ControleDeDisco.java
@@ -1,10 +1,34 @@
 package CotasDeDisco;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ControleDeDisco {
     private long espacoOcupadoBits;
     private String caminhoArquivoEntrada;
 
-    public ControleDeDisco(String caminhoArquivo){
+    public ControleDeDisco() {
+        this("");
+    }
+
+    public ControleDeDisco(String caminhoArquivo) {
         this.caminhoArquivoEntrada = caminhoArquivo;
+    }
+
+    public ArrayList<String[]> readFile(File file) throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+        ArrayList<String[]> usersList = new ArrayList<>();
+
+        while (reader.hasNextLine()) {
+            // \\s+ é um regex para remover os espaços em branco
+            String[] user = reader.nextLine().split("\\s+");
+            usersList.add(user);
+        }
+
+        reader.close();
+        return usersList;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.File;
+
+import CotasDeDisco.ControleDeDisco;
+
 public class Main {
     public static void main(String[] args){
+        ControleDeDisco control = new ControleDeDisco();
 
+        File file = new File("usuarios.txt");
+        
+        try {
+            System.out.println(control.readFile(file));   
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 }
